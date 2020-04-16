cf7f2241a3dc3d38be61f535217307c7f6f7791e
Joseph Rex <joerexko@gmail.com>
2020-04-15 22:10:36 -0500

    Setup minimal gatsby-theme-blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `StyleQ`,
+    description: `The styleQ blog.`,
+    author: `Joseph Rex`,
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/bookstyleq`
+      },
+      {
+        name: `Facebook`,
+        url: `https://www.facebook.com/BookStyleQ/`
+      },
+      {
+        name: `Instagram`,
+        url: `https://www.instagram.com/intostyleq/`
+      }
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,16 +29,22 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `styleq-blog`,
+        short_name: `styleq`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/gatsby-theme-blog/components/bio.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export default () => <p>The amazing blog of styleq</p>
